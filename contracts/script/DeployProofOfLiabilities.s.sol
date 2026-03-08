c12abef1d6086b0130b31332ca4b0d60637336e6
Nuno David <ndavidq0@gmail.com>
2026-03-08 20:51:17 +0000

    chore(contracts): rename deploy scripts

--- contracts/script/DeployProofOfLiabilities.s.sol
+++ contracts/script/DeployProofOfLiabilities.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {ProofOfLiabilities, IVerifier} from "../src/ProofOfLiabilities.sol";
 
-contract VerifierScript is Script {
+contract DeployVerifierScript is Script {
     function run(address owner, IVerifier verifier) public returns (ProofOfLiabilities c) {
         require(owner != address(0));
         require(address(verifier) != address(0));
--- contracts/script/DeployVerifier.s.sol
+++ contracts/script/DeployVerifier.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {HonkVerifier} from "../src/Verifier.sol";
 
-contract VerifierScript is Script {
+contract DeployVerifierScript is Script {
     function run() public returns (HonkVerifier verifier) {
         vm.startBroadcast();
         verifier = new HonkVerifier();
