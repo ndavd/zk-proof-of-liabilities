612d8487e58455bb85a2f4967379eb055318c38e
Nuno David <ndavidq0@gmail.com>
2026-03-08 20:49:26 +0000

    feat(contracts): add deploy scripts

---
+++ contracts/script/ProofOfLiabilities.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {ProofOfLiabilities, IVerifier} from "../src/ProofOfLiabilities.sol";
+
+contract VerifierScript is Script {
+    function run(address owner, IVerifier verifier) public returns (ProofOfLiabilities c) {
+        require(owner != address(0));
+        require(address(verifier) != address(0));
+        vm.startBroadcast();
+        c = new ProofOfLiabilities(owner, verifier);
+        vm.stopBroadcast();
+    }
+}
